21d003fb9e85602133df563a1daf77a8a732629d
Bealth Guy <102649537+Kyle6012@users.noreply.github.com>
2024-01-23 08:03:12 +0300

    Update lib

---
+++ Gallery/lib/weeb.js
@@ -0,0 +1,20 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const weebmenu=`
+「 *Wave BOT* 」
+♡ۣۜۜ፝͜͜͡͡✿➤ *INFO BOT*
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Name: Wave-MD
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Version: 1.0
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Type: VPS
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Owner: Bealth Guy
+  ♡ۣۜۜ፝͜͜͡͡✿➤ wa.me/919931122319
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Prefix: 「  ${prefix}  」
+ ♡ۣۜۜ፝͜͜͡͡✿➤Menu Type: 𝑾𝒆𝒆𝒃-𝒎𝒆𝒏𝒖
+ ----------------------------------
+*Weeb Commands* 
+⌬${prefix}crossplay
+⌬${prefix}neko
+⌬${prefix}loli
+⌬${prefix}wallpaper
+⌬${prefix}waifu
