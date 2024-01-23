29332b279a691cbba8518b62be428dd61d80c824
Bealth Guy <102649537+Kyle6012@users.noreply.github.com>
2024-01-23 07:04:54 +0300

    Update lib

---
+++ Gallery/lib/Group.js
@@ -0,0 +1,25 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const groupmenu=`
+「 *WAVE BOT* 」
+┌──⊰ _*GROUP*_
+│⊳  ${prefix}antilink
+│⊳  ${prefix}closetime
+│⊳  ${prefix}opentime
+│⊳  ${prefix}kick
+│⊳  ${prefix}promote
+│⊳  ${prefix}demote
+│⊳  ${prefix}setdesc
+│⊳  ${prefix}setppgc
+│⊳  ${prefix}tagall
+│⊳  ${prefix}hidetag
+│⊳  ${prefix}totag
+│⊳  ${prefix}group *[option]*
+│⊳  ${prefix}editinfo
+│⊳  ${prefix}gclink
+│⊳  ${prefix}revoke
+│⊳  ${prefix}listonline
+└──────────⊰
+ `
+ module.exports = groupmenu
